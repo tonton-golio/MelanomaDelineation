cc8620815bfc334bf0ecdf56832f8a07ac478e82
Alice Fracchia <139552872+alicefracchia@users.noreply.github.com>
2024-04-06 22:33:47 +0200

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from scipy import signal
-import scripts.tumour_for_streamlit as t
 
 data_path = "./data/"
 sample_path = data_path + "your_sample/"

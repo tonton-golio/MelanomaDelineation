cb55d2298bef5eeb16faee0bbefcc94f33fef6cd
Alice Fracchia <139552872+alicefracchia@users.noreply.github.com>
2024-04-06 22:37:28 +0200

    Update 3_The_convolutional_network.py

--- pages/3_The_convolutional_network.py
+++ pages/3_The_convolutional_network.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
-from sklearn.cluster import KMeans
 from tqdm import tqdm
 
 # set paths

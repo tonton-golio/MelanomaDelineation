394206d1634e90748b7ca66d7a98fe155752d155
Alice Fracchia <139552872+alicefracchia@users.noreply.github.com>
2024-04-07 21:26:28 +0200

    update file

--- Introduction.py
+++ Introduction.py
@@ -17,30 +17,30 @@ def plot_pai_us(pai, us, s, w):
     
 # Title
 st.set_page_config(layout="wide")
-st.title('Your melanoma analysis framework')
+st.title('Automatic delineation framework for melanoma detection')
 
 ccols = st.columns([3, 1])
 
 with ccols[0]:
     """
-    This webapp presents a clearer overview of the processing framework step by step, allowing the user to interact with the sample and fully understand my algorithm.
+    In this webapp you will find an overview of the full algorithm step by step, allowing the user to interact with the sample and tweak the algorithm's parameters.
     """
 with ccols[1]:
     st.image('./data/setup.png')
 
 with st.sidebar:
-    "Select a sample to process and find out how deep is the tumour!"
+    "Select a sample to process and find out how thick is the tumour!"
     # Sample choice
-    q = st.selectbox("Pick a sample!", ['018'])
+    q = st.selectbox("Pick a sample!", ['demo_sample'])
 
 st.divider()
 
-st.subheader("Take a peak at the raw data first...")
-st.write("The 3-dimensional sample con be visualised slice by slice for an easier analysis. Pick a slice and wavelength to look at, and run!")
+st.subheader("- Take a peak at the photoacoustic (PA) and ultrasound imaging data (US) -")
+st.write("Measurements are acquired through consecutive cross imaging sections: move the slider to select which of the slices to visualise and run! Then click on pre-process data to segment the backgound noise from the sample.")
 
 cols = st.columns(2)
 with cols[0]:
-    s = st.slider("Sample's cross-section:", 0, 36, key=1)
+    s = st.slider("Slice:", 0, 36, key=1)
     run = st.toggle('RUN', key=2) 
 with cols[1]:
     wave = st.slider("PA Wavelength [nm]:", 670, 960, step=5, key=3)

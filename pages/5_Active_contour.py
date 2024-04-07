815593cf071705486d4af8b25113c6f3e0d1ad94
Alice Fracchia <139552872+alicefracchia@users.noreply.github.com>
2024-04-07 13:52:45 +0200

    update

--- pages/5_Active_contour.py
+++ pages/5_Active_contour.py
@@ -27,9 +27,9 @@ with cols_[1]:
 if run == True:
     st.video(pre_path + 'active_contour.mp4')
 
-tryit = st.toggle(f'Try it yourself!', key=51)
-cols_ = st.columns([3, 1])
+    tryit = st.toggle(f'Try it yourself!', key=51)
+    cols_ = st.columns([3, 1])
 
-with cols_[1]:
-    if tryit == True:
-        st.image('./data/qr.png')
+    with cols_[1]:
+        if tryit == True:
+            st.image('./data/qr.png')

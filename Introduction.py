c51ce7ff4c39c696a16d98de26e5d15d95252562
Alice Fracchia <139552872+alicefracchia@users.noreply.github.com>
2024-04-07 02:01:10 +0200

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -16,6 +16,7 @@ pre_path = "./data/pre_run/"
 def load_raw_data(s, path=pre_path):
 
     pai = np.load(path + f"pai_reduced_{s}.npy")
+    pai = repeat_values(pai)
     us = np.load(path + f"us_{s}.npy")
 
     return pai, us
@@ -23,6 +24,7 @@ def load_raw_data(s, path=pre_path):
 def pre_processing(path=pre_path):
 
     pai_ready = np.load(path + f"pai_ready_reduced_{s}.npy")
+    pai_ready = repeat_values(pai_ready)
     us_ready = np.load(path + f"us_ready_{s}.npy")
     st.success('Data has been processed.')
 

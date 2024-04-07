40e9508afefd274d8dd4bf5400723c9c7091c05e
Alice Fracchia <139552872+alicefracchia@users.noreply.github.com>
2024-04-07 14:14:57 +0200

    update

--- pages/5_Active_contour.py
+++ pages/5_Active_contour.py
@@ -5,6 +5,9 @@ from matplotlib.colors import LogNorm
 from tqdm import tqdm
 import base64
 
+def repeat_values(x_reduced, repeat_factor=4):
+    return np.repeat(np.repeat(x_reduced, repeats=repeat_factor, axis=0), repeats=repeat_factor, axis=1)
+    
 # path stuff
 data_path = "./data/"
 sample_path = data_path + "your_sample/"
@@ -31,7 +34,14 @@ if run == True:
         st.video(pre_path + 'active_contour.mp4', loop = True)
 
     with colls[1]:
-        st.write('#')
+        pai_ready = np.load(pre_path + f'pai_ready_reduced_{s}.npy')
+        t = np.load(pre_path + 'tumour_area.npy')
+        pai_ready = repeat_values(pai_ready)
+        fig, ax = plt.subplots(figsize=(5, 6))
+        plt.axis('off')
+        plt.imshow(pai_ready[:, :, 0]/pai_ready[:, :, 0].max(), cmap='gray', norm=LogNorm(clip=True), aspect='auto') 
+        plt.scatter(t[:, 1], t[:, 0], s = 0.5, color = 'orangered')
+        st.pyplot(fig, use_container_width=True)
 
     tryit = st.toggle(f'Try it yourself!', key=51)
     cols_ = st.columns([3, 1])

87e44f5c9ed5054234cdb506638be77ab789f2e0
Alice Fracchia <139552872+alicefracchia@users.noreply.github.com>
2024-04-07 12:49:30 +0200

    update file

--- pages/5_Active_contour.py
+++ pages/5_Active_contour.py
@@ -3,17 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from tqdm import tqdm
+import base64
 
 # path stuff
 data_path = "./data/"
 sample_path = data_path + "your_sample/"
-training_path = data_path + "training_data/"
 pre_path = "./data/pre_run/"
-
-@st.cache_data
-def load_gif():
-    gif = np.load(training_path + "contour_gif_20.npy")
-    return gif
 
 # title and layout
 st.set_page_config(layout="wide")
@@ -28,5 +23,11 @@ with cols_[0]:
 with cols_[1]:
     st.write('#')
     run = st.toggle(f'RUN', key=41)
+
+file = open(pre_path, 'rb')
+contents = file.read()
+data_url = base64.b64encode(contents).decode('utf-8-sig')
+file.close()
+st.markdown(f'<img src="data:image/gif;base64,{data_url}>',unsafe_allow_html = True)
 
 st.image('./data/qr.png')
